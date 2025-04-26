e45b3b03aa6c71d8979fdbc2389b5c77702abdf6
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-04-26 14:44:37 +0530

    xcouples.py

--- SHUKLAMUSIC/plugins/tools/couples.py
+++ SHUKLAMUSIC/plugins/tools/couples.py
@@ -41,7 +41,7 @@ def dt_tom():
 tomorrow = str(dt_tom())
 today = str(dt()[0])
 
-@app.on_message(filters.command("couples"))
+@app.on_message(filters.command("xcouples"))
 async def ctest(_, message):
     cid = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
