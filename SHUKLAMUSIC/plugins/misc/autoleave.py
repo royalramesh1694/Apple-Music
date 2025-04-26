b2e01214d6db9e8f14e9062531dc0a94e74959ce
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-04-26 19:57:02 +0530

    autoleave.py

--- SHUKLAMUSIC/plugins/misc/autoleave.py
+++ SHUKLAMUSIC/plugins/misc/autoleave.py
@@ -30,8 +30,8 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOGGER_ID
-                                and i.chat.id != -1002006121442
-                                and i.chat.id != -1001939309491
+                                and i.chat.id != -1001735663878
+                                and i.chat.id != -1001735663878
                             ):
                                 if left == 20:
                                     continue
