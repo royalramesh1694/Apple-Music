b03207524003dd627f8edd717028f5bc3319852e
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-04-26 20:38:11 +0530

    skip.py

--- SHUKLAMUSIC/plugins/admins/skip.py
+++ SHUKLAMUSIC/plugins/admins/skip.py
@@ -120,7 +120,7 @@ async def skip(cli, message: Message, _, chat_id):
             photo=img,
             caption=_["stream_1"].format(
                 f"https://t.me/{app.username}?start=info_{videoid}",
-                title[:23],
+                title[:15],
                 check[0]["dur"],
                 user,
             ),
@@ -153,7 +153,7 @@ async def skip(cli, message: Message, _, chat_id):
             photo=img,
             caption=_["stream_1"].format(
                 f"https://t.me/{app.username}?start=info_{videoid}",
-                title[:23],
+                title[:15],
                 check[0]["dur"],
                 user,
             ),
@@ -196,7 +196,7 @@ async def skip(cli, message: Message, _, chat_id):
                 if str(streamtype) == "audio"
                 else config.TELEGRAM_VIDEO_URL,
                 caption=_["stream_1"].format(
-                    config.SUPPORT_CHAT, title[:23], check[0]["dur"], user
+                    config.SUPPORT_CHAT, title[:15], check[0]["dur"], user
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
@@ -209,7 +209,7 @@ async def skip(cli, message: Message, _, chat_id):
                 if str(streamtype) == "audio"
                 else config.TELEGRAM_VIDEO_URL,
                 caption=_["stream_1"].format(
-                    config.SUPPORT_CHAT, title[:23], check[0]["dur"], user
+                    config.SUPPORT_CHAT, title[:15], check[0]["dur"], user
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
@@ -222,7 +222,7 @@ async def skip(cli, message: Message, _, chat_id):
                 photo=img,
                 caption=_["stream_1"].format(
                     f"https://t.me/{app.username}?start=info_{videoid}",
-                    title[:23],
+                    title[:15],
                     check[0]["dur"],
                     user,
                 ),
