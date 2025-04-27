1e702182500656adb7495dcfaf76e2c29220f22a
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-04-27 17:43:58 +0530

    queue.py

--- SHUKLAMUSIC/utils/inline/queue.py
+++ SHUKLAMUSIC/utils/inline/queue.py
@@ -67,9 +67,6 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
-        ],
-        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
