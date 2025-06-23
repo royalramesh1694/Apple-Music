c470995972bacabb8026ae11c62b32e63575362c
royalramesh1694 <royalramesh1694@gmail.com>
2025-06-23 21:32:59 +0530

    config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/rajeshrakis/Apple-Music",
+    "https://github.com/royalramesh1694//Apple-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
